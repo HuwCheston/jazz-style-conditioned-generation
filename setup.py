414c6711045f4dfe40c2686fcfffd2c4382959d4
Huw Cheston <hwc31@cam.ac.uk>
2025-02-14 09:49:18 +0000

    Added __init__.py and setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='jazz_style_conditioned_generation',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Style-Conditioned Symbolic Jazz Generation',
+    author='Huw Cheston',
+    license='MIT',
+)
