d973a9d30f11f74d755a382bd3f1265143cab9ec
Huw Cheston <hwc31@cam.ac.uk>
2025-02-21 17:41:10 +0000

    Add some __init__s

---
+++ jazz_style_conditioned_generation/data/__init__.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Data classes to be used in training"""
+
+from .conditions import (
+    validate_conditions, get_mapping_for_condition, get_special_tokens_for_condition
+)
+from .dataloader import (
+    DATA_DIR, DatasetMIDICondition
+)
+from .splits import SPLIT_DIR, SPLIT_TYPES, check_all_splits_unique
+from .tokenizer import (
+    get_tokenizer, DEFAULT_TOKENIZER_CLASS, DEFAULT_TRAINING_METHOD, DEFAULT_TOKENIZER_CONFIG
+)
+
+__all__ = [
+    "validate_conditions",
+    "get_mapping_for_condition",
+    "get_special_tokens_for_condition",
+    "DATA_DIR",
+    "DatasetMIDICondition",
+    "get_tokenizer",
+    "DEFAULT_TOKENIZER_CONFIG",
+    "DEFAULT_TRAINING_METHOD",
+    "DEFAULT_TOKENIZER_CLASS",
+    "SPLIT_TYPES",
+    "SPLIT_DIR",
+    "check_all_splits_unique"
+]
