a7db0ed2cd79149efa86cfa6c3e539856529089a
Huw Cheston <hwc31@cam.ac.uk>
2025-02-28 17:20:01 +0000

    (Mostly) working Music Transformer integration

--- jazz_style_conditioned_generation/encoders/music_transformer.py
+++ jazz_style_conditioned_generation/encoders/music_transformer.py
@@ -5,13 +5,27 @@
 
 import math
 import random
+from dataclasses import dataclass
+from typing import Optional, Tuple
 
 import torch
 import torch.nn as nn
 from torch.nn.modules.normalization import LayerNorm
+from transformers.utils import ModelOutput
 
 from jazz_style_conditioned_generation import utils
 from jazz_style_conditioned_generation.encoders.rpr import TransformerEncoderRPR, TransformerEncoderLayerRPR
+
+
+@dataclass
+class MusicTransformerOutput(ModelOutput):
+    """For consistency with the `transformers` API"""
+    loss: Optional[torch.FloatTensor] = None
+    logits: torch.FloatTensor = None
+    past_key_values: Optional[Tuple[Tuple[torch.FloatTensor]]] = None
+    hidden_states: Optional[Tuple[torch.FloatTensor, ...]] = None
+    attentions: Optional[Tuple[torch.FloatTensor, ...]] = None
+    cross_attentions: Optional[Tuple[torch.FloatTensor, ...]] = None
 
 
 class MusicTransformer(nn.Module):
@@ -43,10 +57,8 @@ class MusicTransformer(nn.Module):
 
         # Input embedding
         self.embedding = nn.Embedding(self.tokenizer.vocab_size, self.d_model)
-
         # Positional encoding
         self.positional_encoding = PositionalEncoding(self.d_model, self.dropout, self.max_seq)
-
         # Base transformer
         if not self.rpr:
             # To make a decoder-only transformer we need to use masked encoder layers
@@ -83,40 +95,43 @@ class MusicTransformer(nn.Module):
                 custom_decoder=self.dummy,
                 custom_encoder=encoder
             )
-
         # Final output is a softmaxed linear layer
         self.Wout = nn.Linear(self.d_model, self.tokenizer.vocab_size)
         self.softmax = nn.Softmax(dim=-1)
+        # Calculate loss internally within the class, as in `transformers`
+        self.loss = nn.CrossEntropyLoss(ignore_index=self.tokenizer.pad_token_id)
 
-    def forward(self, x, mask=True, **_):
+    def forward(self, input_ids, labels, attention_mask):
         """Takes an input sequence and outputs predictions using a sequence to sequence method."""
-
-        if mask is True:
-            mask = self.transformer.generate_square_subsequent_mask(x.shape[1]).to(utils.DEVICE)
-        else:
-            mask = None
-
-        x = self.embedding(x)
-
+        # Create causal mask
+        mask = self.transformer.generate_square_subsequent_mask(input_ids.shape[1]).to(utils.DEVICE)
+        x = self.embedding(input_ids)
         # Input shape is (max_seq, batch_size, d_model)
         x = x.permute(1, 0, 2)
-
         x = self.positional_encoding(x)
-
         # Since there are no true decoder layers, the tgt is unused
         # Pytorch wants src and tgt to have some equal dims however
-        x_out = self.transformer(src=x, tgt=x, src_mask=mask)
-
+        x_out = self.transformer(
+            src=x,
+            tgt=x,
+            src_mask=mask,
+            src_key_padding_mask=attention_mask  # masks PAD tokens (i.e., to ensure we have sequence length)
+        )
         # Back to (batch_size, max_seq, d_model)
         x_out = x_out.permute(1, 0, 2)
-
-        y = self.Wout(x_out)
-        # y = self.softmax(y)
-
-        del mask
-
-        # They are trained to predict the next note in sequence (we don't need the last one)
-        return y
+        # Compute logits from FC layer
+        logits = self.Wout(x_out)  # No softmax as nn.CrossEntropyLoss computes it for us
+        # Flatten logits to (sequence_len * batch_size, vocab_size)
+        y = logits.reshape(logits.shape[0] * logits.shape[1], -1).to(torch.float32)
+        # Flatten targets to (sequence_len * batch_size)
+        t = labels.flatten().to(torch.int64)
+        # Compute cross-entropy loss
+        loss = self.loss(y, t)
+        # Returns output in the same format as transformers
+        return MusicTransformerOutput(
+            loss=loss,
+            logits=logits
+        )
 
     def generate(self, primer=None, target_seq_length=1024, beam=0, beam_chance=1.0):
         """Generates midi given a primer sample."""
@@ -138,6 +153,7 @@ class MusicTransformer(nn.Module):
         cur_i = num_primer
         while cur_i < target_seq_length:
             # gen_seq_batch     = gen_seq.clone()
+            # TODO: this is broken
             y = self.softmax(self.forward(gen_seq[..., :cur_i]))[..., :self.tokenizer["EOS_None"]]
             token_probs = y[:, cur_i - 1, :]
 
@@ -188,7 +204,12 @@ class DummyDecoder(nn.Module):
 class PositionalEncoding(nn.Module):
     """Positional encoding, from https://pytorch.org/tutorials/beginner/transformer_tutorial.html"""
 
-    def __init__(self, d_model, dropout=0.1, max_len=5000):
+    def __init__(
+            self,
+            d_model: int,
+            dropout: float = 0.1,
+            max_len: int = utils.MAX_SEQUENCE_LENGTH
+    ):
         super(PositionalEncoding, self).__init__()
         self.dropout = nn.Dropout(p=dropout)
         pe = torch.zeros(max_len, d_model)
@@ -202,3 +223,19 @@ class PositionalEncoding(nn.Module):
     def forward(self, x):
         x = x + self.pe[:x.size(0), :]
         return self.dropout(x)
+
+
+if __name__ == "__main__":
+    from miditok import REMI
+    from jazz_style_conditioned_generation.data.dataloader import create_padding_mask
+
+    token_factory = REMI()
+    mt = MusicTransformer(token_factory, max_sequence=2048).to(utils.DEVICE)
+    dummy_tensor = torch.randint(0, 100, (4, 2049))
+
+    inp = dummy_tensor[:, :-1].to(utils.DEVICE)
+    targ = dummy_tensor[:, 1:].to(utils.DEVICE)
+    padding_mask = create_padding_mask(dummy_tensor, token_factory.pad_token_id)[:, :-1].to(utils.DEVICE)
+
+    out = mt(inp, targ, padding_mask)
+    print(f'Dummy loss: {out.loss.item()}')
