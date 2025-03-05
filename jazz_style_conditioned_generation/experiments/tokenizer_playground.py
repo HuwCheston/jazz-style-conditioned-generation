424e44cc3869d02518263f8086721c58cd145c4d
Huw Cheston <hwc31@cam.ac.uk>
2025-03-05 13:01:59 +0000

    Tokenizer playground pulls from previously-saved results

--- jazz_style_conditioned_generation/experiments/tokenizer_playground.py
+++ jazz_style_conditioned_generation/experiments/tokenizer_playground.py
@@ -19,8 +19,12 @@ TOKENIZER_TYPES = ["structured", "pertok"]
 VOCAB_SIZES = [500, 750, 1000, 2500, 5000, 7500, 10000, 20000]
 TEST_CONFIGS = sorted(list(product(TOKENIZER_TYPES, VOCAB_SIZES)), key=lambda x: x[1])
 
+OUTPUT_CSV = os.path.join(utils.get_project_root(), "references/vocab_size_experiment.csv")
+
 N_EPOCHS_PER_CONFIG = 3
 
+# This configuration will be used in every training run
+#  the only thing different is the tokenizer configuration
 TRAINING_CONFIG = {
     "experiment": "tokenizer-playground",
     "conditions": [],
@@ -84,21 +88,56 @@ def init_training_module(tokenizer_type: str, vocab_size: int) -> TrainingModule
     return TrainingModule(**cfg)
 
 
+def load_existing_results() -> list:
+    """Tries to load existing results as a CSV, returns a list (of dictionaries)"""
+    try:
+        df = pd.read_csv(OUTPUT_CSV)
+    except FileNotFoundError:
+        return []
+    else:
+        return df.to_dict(orient="records")
+
+
+def do_training(training_module: TrainingModule, epoch_num: int) -> dict:
+    """Does training for a single epoch, returns metrics as a dictionary"""
+    start_time = time()
+    training_module.current_epoch = epoch_num
+    # Training
+    train_loss, train_accuracy = training_module.training(epoch_num)
+    logger.debug(f'Epoch {epoch_num} / {training_module.epochs}, training finished: '
+                 f'loss {train_loss:.3f}, accuracy {train_accuracy:.3f}')
+    # Validation
+    validation_loss, validation_accuracy = training_module.validation(epoch_num)
+    logger.debug(f'Epoch {epoch_num} / {training_module.epochs}, validation finished: '
+                 f'loss {validation_loss:.3f}, accuracy {validation_accuracy:.3f}')
+    return {
+        "epoch": epoch_num,
+        "epoch_time": time() - start_time,
+        "train_loss": train_loss,
+        "train_accuracy": train_accuracy,
+        "validation_loss": validation_loss,
+        "validation_accuracy": validation_accuracy
+    }
+
+
 def main(tokenizers: list[str], vocabs: list[str], train_model: bool):
     logger.info("----------TOKENIZER PLAYGROUND----------")
+
     # Get the product of all tokenizers and vocab types, sort by vocab type (increasing)
     test_configs = sorted(list(product(tokenizers, vocabs)), key=lambda x: x[1])
     # Log the total number of experiments
     total = len(test_configs)
     logger.info(f"----------RUNNING {total} EXPERIMENTS----------")
     logger.info(f'EXPERIMENT CONFIGS: {test_configs}')
+
     # We'll use this to store the results if we're training the model
-    res = []
+    res = load_existing_results()
+
     # Iterate through all the experiments we want to do
     for num, (tok_type, vocab_size) in enumerate(test_configs, 1):
+        # Coerce string to an integer
         if isinstance(vocab_size, str):
             vocab_size = int(vocab_size)
-
         logger.info(f"----------EXPERIMENT {num}/{total}: TOK_TYPE {tok_type}, VOCAB_SIZE {vocab_size}------------")
         # Grab the training module, this will also train the tokenizer for us
         tm = init_training_module(tok_type, vocab_size)
@@ -107,31 +146,30 @@ def main(tokenizers: list[str], vocabs: list[str], train_model: bool):
             continue
         # Iterate over all the epochs
         for epoch in range(tm.current_epoch, N_EPOCHS_PER_CONFIG):
-            start_time = time()
-            tm.current_epoch = epoch
-            # Training
-            train_loss, train_accuracy = tm.training(epoch)
-            logger.debug(f'Epoch {epoch} / {tm.epochs}, training finished: '
-                         f'loss {train_loss:.3f}, accuracy {train_accuracy:.3f}')
-            # Validation
-            validation_loss, validation_accuracy = tm.validation(epoch)
-            logger.debug(f'Epoch {epoch} / {tm.epochs}, validation finished: '
-                         f'loss {validation_loss:.3f}, accuracy {validation_accuracy:.3f}')
-            # Append everything to the results list
-            res.append({
-                "tok_type": tok_type,
-                "vocab_size": vocab_size,
-                "epoch": epoch,
-                "epoch_time": time() - start_time,
-                "train_loss": train_loss,
-                "train_accuracy": train_accuracy,
-                "validation_loss": validation_loss,
-                "validation_accuracy": validation_accuracy
-            })
+            res_at_epoch = [
+                i for i in res
+                if (i["epoch"] == epoch) &
+                   (i["tok_type"] == tok_type) &
+                   (i["vocab_size"] == vocab_size)
+            ]
+            # Skip over this epoch if we've already logged results
+            if len(res_at_epoch) > 0:
+                logger.info(f"... skipping epoch {epoch}, tokenizer {tok_type}, vocab {vocab_size}!")
+                continue
+
+            # Do the training for this epoch
+            epoch_metrics = do_training(tm, epoch)
+            # Update the dictionary with a few additional value
+            epoch_metrics["tok_type"] = tok_type
+            epoch_metrics["vocab_size"] = vocab_size
+            # Append the metrics to the results list
+            res.append(epoch_metrics)
+
     # If we've got results from training the models with each tokenizer, dump this to a CSV
     if len(res) > 1:
         df = pd.DataFrame(res)
         df.to_csv(os.path.join(utils.get_project_root(), "references/vocab_size_experiment.csv"))
+
     logger.info('Done!')
 
 
@@ -144,13 +182,15 @@ if __name__ == "__main__":
         '-t', '--tokenizers',
         nargs='+',
         help='Tokenizer types to use',
-        default=TOKENIZER_TYPES
+        default=TOKENIZER_TYPES,
+        type=str
     )
     parser.add_argument(
         '-v', '--vocab-size',
         nargs='+',
         help='Vocab sizes to use',
         default=VOCAB_SIZES,
+        type=int,
     )
     parser.add_argument(
         '-m', '--train-model',
