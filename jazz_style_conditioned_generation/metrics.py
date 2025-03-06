182c84d44f4d21fe85138260a6a31b97f681949c
Huw Cheston <hwc31@cam.ac.uk>
2025-03-06 15:59:29 +0000

    Add metrics and tests for them

---
+++ jazz_style_conditioned_generation/metrics.py
@@ -0,0 +1,282 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Objective metrics for use during evaluation of generated or real examples"""
+
+import os
+import tempfile
+from functools import wraps
+from inspect import getfullargspec
+from pathlib import Path
+from typing import Callable, Any
+
+import muspy
+import numpy as np
+import torch
+from joblib import Parallel, delayed
+from loguru import logger
+from miditok import MusicTokenizer
+from symusic import Score
+from tqdm import tqdm
+
+from jazz_style_conditioned_generation import utils
+from jazz_style_conditioned_generation.data.dataloader import DatasetMIDIExhaustive
+from jazz_style_conditioned_generation.data.tokenizer import load_or_train_tokenizer
+
+
+def _symusic_to_muspy(score: Score) -> muspy.Music:
+    """Converts a symusic.Score object to a muspy.Music object by dumping a temporary MIDI file and cleaning up after"""
+    # Dump the score object to a temporary midi file
+    tempfname = os.path.join(utils.get_project_root(), next(tempfile._get_candidate_names()) + ".mid")
+    score.dump_midi(tempfname)
+    # Load in as a muspy object with the same resolution
+    mp = muspy.read_midi(tempfname)
+    # Sanity check everything
+    assert mp.resolution == score.ticks_per_quarter  # Should have the same resolution
+    assert len(mp.tracks[0].notes) == len(score.tracks[0].notes)  # Should have the same number of notes
+    assert mp.get_end_time() == score.end()  # Should have the same end time
+    # Tidy up by removing the temporary midi file
+    os.remove(tempfname)
+    return mp
+
+
+def coerce_to_muspy(func: Callable):
+    """Coerces input types to a MusPy object, to be used as a decorator"""
+
+    @wraps(func)
+    def _coerce_type(score: Any):
+        if isinstance(score, Score):
+            return func(_symusic_to_muspy(score))
+        elif isinstance(score, str):
+            return func(muspy.read_midi(score))
+        elif isinstance(score, muspy.Music):
+            return func(score)
+        else:
+            raise TypeError(f"Expected `score` to be `symusic.Score`, `muspy.Music`, or `str`, but got {type(score)}")
+
+    return _coerce_type
+
+
+def catch_zero_division(func: Callable):
+    """Little decorator that returns NaN for any function that raises a ZeroDivisionError"""
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ZeroDivisionError:
+            return np.nan
+
+    return wrapper
+
+
+@coerce_to_muspy
+def event_density(score: Any) -> int:
+    """Returns the density of the input in terms of number of notes"""
+    try:
+        return len(score.tracks[0].notes)
+    except IndexError:
+        return 0
+
+
+@coerce_to_muspy
+def pitch_class_entropy(score: Any) -> float:
+    return muspy.pitch_class_entropy(score)
+
+
+@coerce_to_muspy
+def pitch_range(score: Any) -> int:
+    return muspy.pitch_range(score)
+
+
+@coerce_to_muspy
+def polyphony(score: Any) -> int:
+    return muspy.polyphony(score)
+
+
+@coerce_to_muspy
+def number_of_pitches(score: Any) -> int:
+    return muspy.n_pitches_used(score)
+
+
+@coerce_to_muspy
+def number_of_pitch_classes(score: Any) -> int:
+    return muspy.n_pitch_classes_used(score)
+
+
+@catch_zero_division
+def _tone_spans(sorted_pitches: list[int], threshold: int = utils.OCTAVE * 2):
+    above_threshold = 0
+    for p1, p2 in zip(sorted_pitches, sorted_pitches[1:]):
+        if abs(p2 - p1) > threshold:
+            above_threshold += 1
+    return above_threshold / (len(sorted_pitches) - 1)
+
+
+@coerce_to_muspy
+def tone_spans(score: Any) -> float:
+    """Returns the ratio of adjacent notes that have intervals greater than a given threshold"""
+    sorted_notes = sorted(score.tracks[0].notes, key=lambda x: x.time)
+    return _tone_spans([i.pitch for i in sorted_notes])
+
+
+@catch_zero_division
+def _consecutive_repetitions(sorted_pitches: list[int]) -> float:
+    """Given a list of integers corresponding to pitch or p-class, calculate the number of consecutive repetitions"""
+    pitch_repetitions = 0
+    for p1, p2 in zip(sorted_pitches, sorted_pitches[1:]):
+        if p2 == p1:
+            pitch_repetitions += 1
+    return pitch_repetitions / (len(sorted_pitches) - 1)
+
+
+@coerce_to_muspy
+def consecutive_pitch_repetitions(score: Any) -> float:
+    """Returns the ratio of adjacent notes that are repetitions of the same pitch"""
+    sorted_notes = sorted(score.tracks[0].notes, key=lambda x: x.time)
+    return _consecutive_repetitions([i.pitch for i in sorted_notes])
+
+
+@coerce_to_muspy
+def consecutive_pitch_class_repetitions(score: Any) -> float:
+    """Returns the ratio of adjacent notes that are repetitions of the same pitch class"""
+    sorted_notes = sorted(score.tracks[0].notes, key=lambda x: x.time)
+    as_pitch_classes = [i.pitch % utils.OCTAVE for i in sorted_notes]
+    return _consecutive_repetitions(as_pitch_classes)
+
+
+@catch_zero_division
+def rhythmic_variation(token_ids: Any, tokenizer: MusicTokenizer) -> float:
+    """Return the ratio of unique time or duration tokens for a sequence with respect to a tokenizer"""
+    # TODO: see if this is possible to implement?
+    if tokenizer.is_trained:
+        return np.nan
+
+    if isinstance(token_ids, torch.Tensor):
+        token_ids = token_ids.tolist()
+    if len(token_ids) == 0:
+        return 0.
+    # Need to remove the batch dimension here
+    if isinstance(token_ids[0], list):
+        token_ids = token_ids[0]
+    tokenizer_time_values = [i for i in tokenizer.vocab.keys() if "time" in i.lower() or "duration" in i.lower()]
+    tokenized_ids = [tokenizer[i] for i in token_ids]
+    time_tokens = [t for t in tokenized_ids if "time" in t.lower() or "duration" in t.lower()]
+    return len(set(time_tokens)) / len(set(tokenizer_time_values))
+
+
+ALL_METRICS = [
+    event_density,
+    pitch_class_entropy,
+    pitch_range,
+    polyphony,
+    number_of_pitches,
+    number_of_pitch_classes,
+    tone_spans,
+    consecutive_pitch_repetitions,
+    consecutive_pitch_class_repetitions,
+    rhythmic_variation
+]
+ALL_METRIC_NAMES = [
+    "event_density",
+    "pitch_class_entropy",
+    "pitch_range",
+    "polyphony",
+    "number_of_pitches",
+    "number_of_pitch_classes",
+    "tone_spans",
+    "consecutive_pitch_repetitions",
+    "consecutive_pitch_class_repetitions",
+    "rhythmic_variation"
+]
+
+
+def compute_metrics_for_sequence(token_ids: torch.Tensor, tokenizer: MusicTokenizer):
+    """Given a tensor of token ids from a sequence, compute all evaluation metrics"""
+    # Need to add the batch dimension in if this isn't present already
+    if len(tuple(token_ids.size())) == 1:
+        token_ids = token_ids.unsqueeze(0)
+    # We'll store results for this track in here
+    res_for_track = {}
+    # Decode the IDs into a symusic.Score object, then convert this to Muspy
+    decoded = tokenizer.decode(token_ids)
+    # If we decode the tokens into an empty sequence
+    if len(decoded.tracks) == 0:
+        logger.warning(f"Sequence decoded into an empty score. All metrics will be missing! Sequence {token_ids}")
+        # We need to return None for all metrics, so that this sequence will be skipped if we go on to aggregate
+        for func in ALL_METRIC_NAMES:
+            res_for_track[func] = np.nan
+        return res_for_track
+    # Otherwise, we can safely coerce the score to muspy
+    coerced_to_muspy = _symusic_to_muspy(decoded)
+    # Iterate over all the metrics we want to calculate
+    for func_to_call, func in zip(ALL_METRICS, ALL_METRIC_NAMES):
+        func_to_call = utils.get_original_function(func_to_call)
+        needed_args = getfullargspec(func_to_call).args
+        # Function is expecting a muspy.Music object
+        if "score" in needed_args:
+            func_res = func_to_call(coerced_to_muspy)
+        # Function is expecting the raw tokens and the tokenizer
+        elif "token_ids" in needed_args:
+            func_res = func_to_call(token_ids, tokenizer)
+        # Function has an incorrect signature
+        else:
+            raise AttributeError(f"Unknown function signature {needed_args} for function name {func}")
+        res_for_track[func] = func_res
+    return res_for_track
+
+
+def compute_metrics_for_sequences(token_ids_list: list[torch.Tensor], tokenizer: MusicTokenizer) -> dict:
+    """Computes evaluation metrics for multiple sequences in parallel and aggregates into a single dictionary"""
+    # Get values for all metrics from every token id in the list
+    with Parallel(n_jobs=-1) as par:
+        all_res = par(
+            delayed(compute_metrics_for_sequence)(token_ids, tokenizer)
+            for token_ids in tqdm(token_ids_list, desc="Getting evaluation metric results...")
+        )
+    # Aggregate across values obtained from every chunk
+    return aggregate_evaluation_metrics(all_res)
+
+
+def aggregate_evaluation_metrics(evaluation_results: list[dict]) -> dict:
+    """Aggregates evaluation results obtained from multiple sequences into a single dictionary"""
+    all_aggs = {}
+    for k in evaluation_results[0]:
+        k_vals = [i[k] for i in evaluation_results]
+        for agg_name, agg_func in zip(["mean", "std"], [np.nanmean, np.nanstd]):
+            agg_res = agg_func(k_vals)
+            all_aggs[f"{k}_{agg_name}"] = agg_res
+    return all_aggs
+
+
+def compute_metrics_for_dataset(dataset_tracks: list[str], tokenizer: MusicTokenizer, **dataset_kwargs) -> dict:
+    """Given a list of tracks in a dataset and a tokenizer, compute all evaluation metrics and aggregate"""
+    if isinstance(dataset_tracks[0], Path):
+        dataset_tracks = [str(dt) for dt in dataset_tracks]
+    # Create the dataset object, which will chunk every track into non-overlapping chunks of N tokens
+    dataset = DatasetMIDIExhaustive(
+        tokenizer,
+        dataset_tracks,
+        max_seq_len=utils.MAX_SEQUENCE_LENGTH,
+        do_augmentation=False,
+        do_conditioning=False,
+        **dataset_kwargs
+    )
+    # Get values for all metrics from every chunk in the dataset in parallel
+    with Parallel(n_jobs=-1) as par:
+        all_res = par(
+            delayed(compute_metrics_for_sequence)(td["input_ids"], tokenizer)
+            for td in tqdm(dataset, desc="Getting evaluation metric results...")
+        )
+    # Aggregate across values obtained from every chunk
+    return aggregate_evaluation_metrics(all_res)
+
+
+if __name__ == "__main__":
+    token = load_or_train_tokenizer("", {"tokenizer_str": "midilike"}, [])
+    jtd = utils.get_data_files_with_ext("data/raw/jtd", "**/*.mid")[:10]
+    token.train(500, files_paths=jtd)
+    cm = compute_metrics_for_dataset(jtd, token)
+
+    print('Metrics for 10 JTD tracks: ')
+    print(cm)
---
+++ tests/test_metrics.py
@@ -0,0 +1,246 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Test suite for evaluation metrics"""
+
+import os
+import unittest
+
+import numpy as np
+import torch
+from miditok import MIDILike, TokenizerConfig
+from muspy import Music
+from symusic import Note
+
+from jazz_style_conditioned_generation import metrics
+from jazz_style_conditioned_generation import utils
+from jazz_style_conditioned_generation.data.dataloader import note_list_to_score
+
+
+class MetricsTest(unittest.TestCase):
+    def test_to_muspy(self):
+        notes = [
+            Note(pitch=80, time=100, duration=100, velocity=80, ttype="tick"),
+            Note(pitch=70, time=200, duration=100, velocity=80, ttype="tick"),
+            Note(pitch=60, time=300, duration=100, velocity=80, ttype="tick"),
+        ]
+        score = note_list_to_score(notes, ticks_per_quarter=100)
+        muspy = metrics._symusic_to_muspy(score)
+        self.assertTrue(isinstance(muspy, Music))
+        self.assertTrue(muspy.resolution == score.ticks_per_quarter)
+        self.assertTrue(len(muspy.tracks[0].notes) == len(score.tracks[0].notes))
+        self.assertTrue(muspy.get_end_time() == score.end())
+
+    def test_coerce_to_muspy_wrapper(self):
+        @metrics.coerce_to_muspy
+        def func(x):
+            return x
+
+        fp = os.path.join(utils.get_project_root(), "tests/test_resources/test_midi1/piano_midi.mid")
+        self.assertTrue(isinstance(func(fp), Music))
+        notes = [
+            Note(pitch=80, time=100, duration=100, velocity=80, ttype="tick"),
+            Note(pitch=70, time=200, duration=100, velocity=80, ttype="tick"),
+            Note(pitch=60, time=300, duration=100, velocity=80, ttype="tick"),
+        ]
+        score = note_list_to_score(notes, ticks_per_quarter=100)
+        self.assertTrue(isinstance(func(score), Music))
+        self.assertTrue(isinstance(func(Music()), Music))
+        with self.assertRaises(TypeError):
+            _ = func(123)
+            _ = func(0.1)
+            _ = func(True)
+
+    def test_catch_zero_division(self):
+        @metrics.catch_zero_division
+        def good():
+            return 1 / 2
+
+        @metrics.catch_zero_division
+        def bad():
+            return 1 / 0
+
+        self.assertTrue(good() == 0.5)
+        self.assertTrue(np.isnan(bad()))
+
+    def test_event_density(self):
+        notes = [
+            Note(pitch=80, time=100, duration=100, velocity=80, ttype="tick"),
+            Note(pitch=70, time=200, duration=100, velocity=80, ttype="tick"),
+            Note(pitch=60, time=300, duration=100, velocity=80, ttype="tick"),
+        ]
+        score = note_list_to_score(notes, ticks_per_quarter=100)
+        expected = 3
+        actual = metrics.event_density(score)
+        self.assertEqual(expected, actual)
+        self.assertEqual(metrics.event_density(Music()), 0)
+
+    def test_tone_spans(self):
+        # Test with one big leap
+        notes = [
+            Note(pitch=80, time=100, duration=100, velocity=80, ttype="tick"),
+            Note(pitch=30, time=200, duration=100, velocity=80, ttype="tick"),
+            Note(pitch=45, time=300, duration=100, velocity=80, ttype="tick"),
+        ]
+        expected = 1 / 2
+        score = note_list_to_score(notes, ticks_per_quarter=100)
+        actual = metrics.tone_spans(score)
+        self.assertEqual(expected, actual)
+        # Test with no big leaps
+        notes = [
+            Note(pitch=80, time=100, duration=100, velocity=80, ttype="tick"),
+            Note(pitch=70, time=200, duration=100, velocity=80, ttype="tick"),
+            Note(pitch=65, time=300, duration=100, velocity=80, ttype="tick"),
+        ]
+        expected = 0.
+        score = note_list_to_score(notes, ticks_per_quarter=100)
+        actual = metrics.tone_spans(score)
+        self.assertEqual(expected, actual)
+
+    def test_consecutive_pitch_repetitions(self):
+        # Test with one repeat note
+        notes = [
+            Note(pitch=80, time=100, duration=100, velocity=80, ttype="tick"),
+            Note(pitch=80, time=200, duration=100, velocity=80, ttype="tick"),
+            Note(pitch=75, time=300, duration=100, velocity=80, ttype="tick"),
+        ]
+        score = note_list_to_score(notes, ticks_per_quarter=100)
+        expected = 1 / 2
+        actual = metrics.consecutive_pitch_repetitions(score)
+        self.assertEqual(expected, actual)
+        # Test with no repeat notes
+        notes = [
+            Note(pitch=80, time=100, duration=100, velocity=80, ttype="tick"),
+            Note(pitch=70, time=200, duration=100, velocity=80, ttype="tick"),
+            Note(pitch=75, time=300, duration=100, velocity=80, ttype="tick"),
+        ]
+        score = note_list_to_score(notes, ticks_per_quarter=100)
+        expected = 0.
+        actual = metrics.consecutive_pitch_repetitions(score)
+        self.assertEqual(expected, actual)
+        # Test with all repeat notes
+        notes = [
+            Note(pitch=70, time=100, duration=100, velocity=80, ttype="tick"),
+            Note(pitch=70, time=200, duration=100, velocity=80, ttype="tick"),
+            Note(pitch=70, time=300, duration=100, velocity=80, ttype="tick"),
+        ]
+        score = note_list_to_score(notes, ticks_per_quarter=100)
+        expected = 1.
+        actual = metrics.consecutive_pitch_repetitions(score)
+        self.assertEqual(expected, actual)
+
+    def test_consecutive_pitch_class_repetitions(self):
+        # Test with one repeat note
+        notes = [
+            Note(pitch=80, time=100, duration=100, velocity=80, ttype="tick"),
+            Note(pitch=68, time=200, duration=100, velocity=80, ttype="tick"),
+            Note(pitch=75, time=300, duration=100, velocity=80, ttype="tick"),
+        ]
+        score = note_list_to_score(notes, ticks_per_quarter=100)
+        expected = 1 / 2
+        actual = metrics.consecutive_pitch_class_repetitions(score)
+        self.assertEqual(expected, actual)
+        # Test with no repeat notes
+        notes = [
+            Note(pitch=80, time=100, duration=100, velocity=80, ttype="tick"),
+            Note(pitch=70, time=200, duration=100, velocity=80, ttype="tick"),
+            Note(pitch=75, time=300, duration=100, velocity=80, ttype="tick"),
+        ]
+        score = note_list_to_score(notes, ticks_per_quarter=100)
+        expected = 0.
+        actual = metrics.consecutive_pitch_class_repetitions(score)
+        self.assertEqual(expected, actual)
+        # Test with all repeat notes
+        notes = [
+            Note(pitch=80, time=100, duration=100, velocity=80, ttype="tick"),
+            Note(pitch=68, time=200, duration=100, velocity=80, ttype="tick"),
+            Note(pitch=56, time=300, duration=100, velocity=80, ttype="tick"),
+            Note(pitch=56, time=400, duration=100, velocity=80, ttype="tick"),
+            Note(pitch=68, time=500, duration=100, velocity=80, ttype="tick"),
+        ]
+        score = note_list_to_score(notes, ticks_per_quarter=100)
+        expected = 1.
+        actual = metrics.consecutive_pitch_class_repetitions(score)
+        self.assertEqual(expected, actual)
+
+    def test_rhythmic_variation(self):
+        tc = TokenizerConfig(beat_res={(0, 1): 4})  # gives us 4 time-shift tokens
+        tok = MIDILike(tc)
+        # Should return 0 when no notes passed
+        self.assertEqual(metrics.rhythmic_variation([], tok), 0.)
+        # Test with a sequence using one rhythm token
+        seq = [0, 2, 5, 212]
+        expected = 1 / 4
+        actual = metrics.rhythmic_variation(seq, tok)
+        self.assertEqual(expected, actual)
+        # Test with a sequence using two rhythm tokens
+        seq = [1, 3, 3, 3, 212, 213]
+        expected = 1 / 2
+        actual = metrics.rhythmic_variation(seq, tok)
+        self.assertEqual(expected, actual)
+        # Test with a sequence using all of our rhythm tokens
+        seq = [1, 5, 6, 2, 5, 50, 212, 213, 214, 216, 215, 211]
+        expected = 1.
+        actual = metrics.rhythmic_variation(seq, tok)
+        self.assertEqual(expected, actual)
+        # Should just return NaN if the tokenizer is trained
+        tok.train(vocab_size=500, iterator=[])  # will just train immediately
+        seq = [0, 1, 2, 3]
+        self.assertTrue(np.isnan(metrics.rhythmic_variation(seq, tok)))
+
+    def test_compute_metrics_for_sequence(self):
+        tok = MIDILike()
+        # Test with a working sequence
+        seqs = torch.tensor([
+            [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+            [0, 0, 0, 0, 0, 1, 5, 5, 5, 5, 5, ],
+            [6, 6, 6, 6, 6, 7, 7, 6, 6, 6, 6, ]
+        ])
+        parsed = metrics.compute_metrics_for_sequence(seqs, tok)
+        self.assertTrue(isinstance(parsed, dict))
+        for k, v in parsed.items():
+            self.assertTrue(isinstance(k, str))
+            self.assertTrue(isinstance(v, float))
+        # Test with a bad sequence
+        seqs = torch.tensor([[]])
+        parsed = metrics.compute_metrics_for_sequence(seqs, tok)
+        self.assertTrue(isinstance(parsed, dict))
+        for k, v in parsed.items():
+            self.assertTrue(isinstance(k, str))
+            self.assertTrue(np.isnan(v))
+
+    def test_compute_metrics_for_sequences(self):
+        tok = MIDILike()
+        # Test with a working sequence
+        seqs = [
+            torch.tensor([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+            torch.tensor([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+            torch.tensor([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+            torch.tensor([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+        ]
+        parsed = metrics.compute_metrics_for_sequences(seqs, tok)
+        self.assertTrue(isinstance(parsed, dict))
+        for k in metrics.ALL_METRIC_NAMES:
+            self.assertTrue(f'{k}_mean' in list(parsed.keys()))
+            self.assertTrue(f'{k}_std' in list(parsed.keys()))
+
+    def test_aggregate_evaluation_metrics(self):
+        mets = [
+            {
+                "test1": 1.,
+                "test2": 2.,
+            },
+            {
+                "test1": 2.,
+                "test2": 3.,
+            }
+        ]
+        agg = metrics.aggregate_evaluation_metrics(mets)
+        self.assertEqual(agg["test1_mean"], 1.5)
+        self.assertEqual(agg["test2_mean"], 2.5)
+        self.assertEqual(agg["test1_std"], 0.5)
+        self.assertEqual(agg["test2_std"], 0.5)
+
+
+if __name__ == '__main__':
+    unittest.main()
