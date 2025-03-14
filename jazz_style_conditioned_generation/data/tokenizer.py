e5ab9f03dffb6f6e8402a350ab155d0622a56d9d
Huw Cheston <hwc31@cam.ac.uk>
2025-03-14 10:04:08 +0000

    Add custom TokTrainingIterator, remove load_or_train_tokenizer

--- jazz_style_conditioned_generation/data/tokenizer.py
+++ jazz_style_conditioned_generation/data/tokenizer.py
@@ -4,7 +4,6 @@
 """Trains a Tokenizer in MIDITok and dumps a .json with all parameters"""
 
 import os
-from itertools import product
 from pathlib import Path
 from typing import Sequence
 
@@ -15,14 +14,8 @@ from miditok.attribute_controls import create_random_ac_indexes
 from miditok.constants import SCORE_LOADING_EXCEPTION
 from miditok.tokenizations import REMI, MIDILike, TSD, Structured, PerTok
 from miditok.tokenizer_training_iterator import TokTrainingIterator
-from tqdm import tqdm
 
 from jazz_style_conditioned_generation import utils
-from jazz_style_conditioned_generation.data.augmentation import (
-    _data_augmentation_deterministic,
-    PITCH_AUGMENT_RANGE,
-    DURATION_AUGMENT_RANGE
-)
 from jazz_style_conditioned_generation.data.conditions import validate_condition_values
 from jazz_style_conditioned_generation.data.scores import load_score, preprocess_score
 
@@ -34,7 +27,8 @@ DEFAULT_TOKENIZER_CONFIG = {
         "PAD",  # add for short inputs to ensure consistent sequence length for all inputs
         "BOS",  # beginning of sequence
         "EOS",  # end of sequence
-        "MASK",  # prevent attention to future tokens
+        # TODO: is this needed?
+        # "MASK",  # prevent attention to future tokens
     ],
     "use_chords": False,
     "use_rests": False,
@@ -57,8 +51,8 @@ DEFAULT_TOKENIZER_CLASS = "tsd"
 OUTPUT_MIDI_DIR = os.path.join(utils.get_project_root(), 'outputs/midi/tokenized')
 
 
-class TokTrainingIteratorAugmentation(TokTrainingIterator):
-    """Allows a tokenizer to be trained using AUGMENTED versions of MIDI files, generated on the fly"""
+class CustomTokTrainingIterator(TokTrainingIterator):
+    """Modifies miditok.TokTrainingIterator to use our custom Score loading and preprocessing functions"""
 
     def __init__(
             self,
@@ -68,44 +62,19 @@ class TokTrainingIteratorAugmentation(TokTrainingIterator):
             bars_idx_random_ratio_range: tuple[float, float] | None = None,
     ):
         super().__init__(tokenizer, files_paths, tracks_idx_random_ratio_range, bars_idx_random_ratio_range)
-        self.files_paths_with_augs = list(self.get_augs_for_files())
 
-    def get_augs_for_files(self):
-        """Iterates through all files and returns tuples of (filepath, pitch_augmentation, duration_augmentation)"""
-        for f in tqdm(self.files_paths, desc=f"Getting all augmentations for {len(self.files_paths)} tracks..."):
-            # Load as a score object
-            sc = load_score(f)
-            # Apply our preprocessing to remove invalid notes, merge repeated notes etc.
-            preproc = preprocess_score(sc)
-            # Get the minimum and maximum pitch of the preprocessed scores
-            min_pitch, max_pitch = utils.get_pitch_range(preproc)
-            # Get the range of potential pitch augmentations that can be applied to this score
-            pitch_augs = [
-                i for i in PITCH_AUGMENT_RANGE
-                if max_pitch + i <= utils.MIDI_OFFSET + utils.PIANO_KEYS
-                   and min_pitch + i >= utils.MIDI_OFFSET
-            ]
-            # Iterate over all the possible augmentations we can apply to the score and yield a tuple
-            for pitch_aug, duration_aug in product(pitch_augs, DURATION_AUGMENT_RANGE):
-                yield f, pitch_aug, duration_aug
-
-    def load_file_with_aug(self, path: Path, pitch_aug: int, dur_aug: float) -> list[str]:
-        """Load a file, apply augmentation, and convert to a byte representation"""
-        # Load and tokenize file
+    def load_file(self, path: Path) -> list[str]:
+        """Load a file and preprocess with our custom functions, then convert to a byte representation"""
+        # Load the score using our custom loading function
         try:
             score = load_score(path)
         except SCORE_LOADING_EXCEPTION:
             return []
-
         # Apply our own preprocessing to the score
         score = preprocess_score(score)
-        # Apply the desired augmentations to the score
-        score = _data_augmentation_deterministic(score, pitch_aug, dur_aug)
-
-        # Everything below is copied from MIDITok.tokenizer_training_iterator.TokTrainingIterator
+        # Stuff below is copied from MIDITok.tokenizer_training_iterator.TokTrainingIterator unless indicated
         # Preprocess first to already have the appropriate tracks idx in case of deletes
         score = self.tokenizer.preprocess_score(score)
-
         # Randomly create attribute controls indexes
         ac_indexes = None
         if (
@@ -118,35 +87,15 @@ class TokTrainingIteratorAugmentation(TokTrainingIterator):
                 self.tracks_idx_random_ratio_range,
                 self.bars_idx_random_ratio_range,
             )
-
         # Tokenize the file
-        # Need to specify `encode_ids=False` as it might be already pretrained
-        # For MMM, we make sure to have sequences separated per track
-        kwargs = {}
-        # can't use isinstance because of circular import
-        if type(self.tokenizer).__name__ == "MMM":
-            kwargs["concatenate_track_sequences"] = False
+        # REMOVED: stuff to do with MMM tokenization
         tokseq = self.tokenizer(
             score,
             encode_ids=False,
             no_preprocess_score=True,
             attribute_controls_indexes=ac_indexes,
-            **kwargs,
         )
-
-        # Split ids if requested
-        if self.tokenizer.config.encode_ids_split in ["bar", "beat"]:
-            if isinstance(tokseq, TokSequence):
-                tokseq = [tokseq]
-
-            new_seqs = []
-            for seq in tokseq:
-                if self.tokenizer.config.encode_ids_split == "bar":
-                    new_seqs += seq.split_per_bars()
-                else:
-                    new_seqs += seq.split_per_beats()
-            tokseq = [seq for seq in new_seqs if len(seq) > 0]
-
+        # REMOVED: splitting IDs (we don't want to do this ever)
         # Convert ids to bytes for training
         if isinstance(tokseq, TokSequence):
             token_ids = tokseq.ids
@@ -155,18 +104,7 @@ class TokTrainingIteratorAugmentation(TokTrainingIterator):
         bytes_ = self.tokenizer._ids_to_bytes(token_ids, as_one_str=True)
         if isinstance(bytes_, str):
             bytes_ = [bytes_]
-
         return bytes_
-
-    def __len__(self):
-        return len(self.files_paths_with_augs)
-
-    def __getitem__(self, idx: int) -> list[str]:
-        return self.load_file_with_aug(*self.files_paths_with_augs[idx])
-
-    def __str__(self) -> str:
-        """Return the ``str`` representation of the iterator."""
-        return f"{self.tokenizer} - {len(self.files_paths)} tracks, {len(self)} augmented tracks"
 
 
 def add_pianists_to_vocab(tokenizer, metadata_paths: list[str]) -> None:
@@ -187,7 +125,7 @@ def add_pianists_to_vocab(tokenizer, metadata_paths: list[str]) -> None:
     # We only care about the name of the genre, not the weight
     for pianist, _ in validated_pianists:
         with_prefix = f'PIANIST_{utils.remove_punctuation(pianist).replace(" ", "")}'
-        tokenizer.add_to_vocab(with_prefix)
+        tokenizer.add_to_vocab(with_prefix, special_token=False)
 
 
 def add_genres_to_vocab(tokenizer: MusicTokenizer, metadata_paths: list[str]) -> None:
@@ -219,14 +157,14 @@ def add_genres_to_vocab(tokenizer: MusicTokenizer, metadata_paths: list[str]) ->
     # We only care about the name of the genre, not the weight
     for genre, _ in validated_genres:
         with_prefix = f'GENRES_{utils.remove_punctuation(genre).replace(" ", "")}'
-        tokenizer.add_to_vocab(with_prefix)
+        tokenizer.add_to_vocab(with_prefix, special_token=False)
 
 
 def add_timesignatures_to_vocab(tokenizer: MusicTokenizer, time_signatures: list[int]) -> None:
     """Given a list of time signatures, add these to the vocabulary as custom tokens (shouldn't be used in decoding)"""
     for time_signature in time_signatures:
         tok_id = f'TIMESIGNATURECUSTOM_{time_signature}4'
-        tokenizer.add_to_vocab(tok_id)
+        tokenizer.add_to_vocab(tok_id, special_token=False)
 
 
 def add_tempos_to_vocab(tokenizer: MusicTokenizer, tempo_range: tuple, n_tempos: int = 32) -> None:
@@ -234,7 +172,7 @@ def add_tempos_to_vocab(tokenizer: MusicTokenizer, tempo_range: tuple, n_tempos:
     tempo_range = np.linspace(*tempo_range, n_tempos).round().astype(int)
     for tempo in tempo_range:
         tok_id = f'TEMPOCUSTOM_{tempo}'
-        tokenizer.add_to_vocab(tok_id)
+        tokenizer.add_to_vocab(tok_id, special_token=False)
 
 
 def get_tokenizer_class_from_string(tokenizer_type: str):
@@ -269,53 +207,30 @@ def fix_pertok_microtiming_bug(tokenizer: PerTok) -> None:
         assert hasattr(tokenizer, "microtiming_tick_values")
 
 
-def load_or_train_tokenizer(
-        tokenizer_path: str,
-        tokenizer_cfg: dict,
-        track_paths: list[str]
-) -> MusicTokenizer:
-    """Tries to load a tokenizer from `tokenizer_path`, trains from scratch if this cannot be found"""
-
+def load_tokenizer(**kwargs) -> MusicTokenizer:
     # Get the variables from the config dictionary
-    tokenizer_method = tokenizer_cfg.get("tokenizer_str", DEFAULT_TOKENIZER_CLASS)
-    tokenizer_kws = tokenizer_cfg.get("tokenizer_kws", DEFAULT_TOKENIZER_CONFIG)
+    tokenizer_method = kwargs.get("tokenizer_str", DEFAULT_TOKENIZER_CLASS)
+    tokenizer_kws = kwargs.get("tokenizer_kws", DEFAULT_TOKENIZER_CONFIG)
     # Add in any missing parameters with defaults
     tokenizer_kws = utils.update_dictionary(tokenizer_kws, DEFAULT_TOKENIZER_CONFIG)
     logger.debug(f'Initialising tokenizer type {tokenizer_method} with params {tokenizer_kws}')
-    # If we've already trained the tokenizer for this run
-    if os.path.isfile(tokenizer_path):
-        tokenizer = get_tokenizer_class_from_string(tokenizer_method)(params=tokenizer_path)
-        logger.debug(f'... loading tokenizer from {tokenizer_path}')
-        # Fix a MIDITok bug related to loading a pre-trained PerTok tokenizer
-        if tokenizer_method == "pertok":
-            fix_pertok_microtiming_bug(tokenizer)
-
-    # Otherwise, we need to create the tokenizer from scratch
-    else:
-        logger.debug('... could not find saved tokenizer for this experiment/run, creating it from scratch!')
-        cfg = TokenizerConfig(**tokenizer_kws)
-        tokenizer = get_tokenizer_class_from_string(tokenizer_method)(cfg)
-        logger.debug(f'... got tokenizer: {tokenizer}')
-        # If we want to train the tokenizer
-        if tokenizer_cfg.get("do_training", False):
-            # Get the parameters again from the dictionary
-            training_method = tokenizer_cfg.get("training_method", DEFAULT_TRAINING_METHOD)
-            vocab_size = tokenizer_cfg.get("vocab_size", DEFAULT_VOCAB_SIZE)
-            logger.debug(f'... training tokenizer with method {training_method}, vocab size {vocab_size}')
-            # TODO: this should use our custom load_score function
-            # TODO: we need to add our condition tokens in BEFORE training the tokenizer or else we'll get errors
-            tokenizer.train(vocab_size=vocab_size, model=training_method, files_paths=track_paths)
-
-            # Create the iterator which we use to train with augmentation
-            # tti = TokTrainingIteratorAugmentation(tokenizer, track_paths)
-            # logger.debug(f'... using iterator: {tti}')
-            # # Train the tokenizer
-            # tokenizer.train(vocab_size=vocab_size, model=training_method, iterator=tti)
-            logger.debug(f'... successfully trained tokenizer')
-        # Finally, we can dump the tokenizer so that we reload it on future runs
-        tokenizer.save(tokenizer_path)
-        logger.debug(f'... tokenizer saved to {tokenizer_path}')
+    # Create the tokenizer configuration + tokenizer
+    cfg = TokenizerConfig(**tokenizer_kws)
+    tokenizer = get_tokenizer_class_from_string(tokenizer_method)(cfg)
+    logger.debug(f'... got tokenizer: {tokenizer}')
     return tokenizer
+
+
+def train_tokenizer(tokenizer: MusicTokenizer, files_paths: list[str], **kwargs) -> None:
+    """Trains a tokenizer given kwargs using our custom iterator class"""
+    # Get the parameters again from the dictionary
+    training_method = kwargs.get("training_method", DEFAULT_TRAINING_METHOD)
+    vocab_size = kwargs.get("vocab_size", DEFAULT_VOCAB_SIZE)
+    logger.debug(f'... training tokenizer with method {training_method}, vocab size {vocab_size}')
+    # We need to train with our custom iterator so that we use our custom score loading + preprocessing functions
+    tti = CustomTokTrainingIterator(tokenizer, files_paths)
+    tokenizer.train(vocab_size=vocab_size, model=training_method, iterator=tti)
+    logger.debug(f'... training finished: {tokenizer}')
 
 
 if __name__ == "__main__":
--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -15,9 +15,6 @@ from jazz_style_conditioned_generation.data.tokenizer import (
     add_pianists_to_vocab,
     add_genres_to_vocab,
     get_tokenizer_class_from_string,
-    TokTrainingIteratorAugmentation,
-    load_or_train_tokenizer,
-    DEFAULT_TOKENIZER_CLASS,
     DEFAULT_TOKENIZER_CONFIG
 )
 
@@ -30,39 +27,14 @@ class TokenizerTest(unittest.TestCase):
             actual = get_tokenizer_class_from_string(name)
             self.assertEqual(actual, ty)
 
-    def test_training_iterator(self):
-        token_factory = REMI()
-        fps = [os.path.join(utils.get_project_root(), "tests/test_resources/test_midi1/piano_midi.mid")]
-        tti = TokTrainingIteratorAugmentation(token_factory, fps)
-        # We should only have one track, but we should be augmenting it multiple times
-        self.assertEqual(len(tti.files_paths), 1)
-        self.assertGreater(len(tti), 1)
-        # We shouldn't be splitting IDs, so we should only get one item per track
-        token_factory.config.encode_ids_split = "no"
-        gotter = tti.__getitem__(0)
-        self.assertEqual(len(gotter), 1)
-        # Now we can try with splitting
-        token_factory.config.encode_ids_split = "bar"
-        gotter = tti.__getitem__(0)
-        self.assertGreater(len(gotter), 1)
+    def test_get_tokenizer(self):
+        pass
 
-    def test_load_or_train_tokenizer(self):
-        fps = [os.path.join(utils.get_project_root(), "tests/test_resources/test_midi1/piano_midi.mid")]
-        # Test without training
-        actual = load_or_train_tokenizer("tmp.json", dict(do_training=False), fps)
-        self.assertTrue(isinstance(actual, get_tokenizer_class_from_string(DEFAULT_TOKENIZER_CLASS)))
-        self.assertFalse(actual.is_trained)
-        # We should dump the tokenizer to a json
-        self.assertTrue(os.path.isfile("tmp.json"))
-        os.remove("tmp.json")
+    def test_train_tokenizer(self):
+        pass
 
-        # Test with training
-        actual = load_or_train_tokenizer("tmp.json", dict(do_training=True), fps)
-        self.assertTrue(isinstance(actual, get_tokenizer_class_from_string(DEFAULT_TOKENIZER_CLASS)))
-        self.assertTrue(actual.is_trained)
-        # We should dump the tokenizer to a json
-        self.assertTrue(os.path.isfile("tmp.json"))
-        os.remove("tmp.json")
+    def test_custom_training_iterator(self):
+        pass
 
     def test_default_tokenizer_config(self):
         tok = MIDILike(TokenizerConfig(**DEFAULT_TOKENIZER_CONFIG))
@@ -82,9 +54,10 @@ class TokenizerTest(unittest.TestCase):
         add_tempos_to_vocab(tok, (100, 200), 11)
         expected_vocab = prev_vocab + 11
         self.assertEqual(tok.vocab_size, expected_vocab)
-        self.assertTrue("TEMPOCUSTOM_100" in tok.vocab.keys())
-        self.assertTrue("TEMPOCUSTOM_150" in tok.vocab.keys())
-        self.assertTrue("TEMPOCUSTOM_200" in tok.vocab.keys())
+        expected = ["TEMPOCUSTOM_100", "TEMPOCUSTOM_150", "TEMPOCUSTOM_200"]
+        for expect in expected:
+            self.assertTrue(expect in tok.vocab)
+            # self.assertTrue(expect in tok.special_tokens)
 
     def test_add_timesignatures(self):
         tok = MIDILike(TokenizerConfig(**DEFAULT_TOKENIZER_CONFIG))
@@ -93,9 +66,10 @@ class TokenizerTest(unittest.TestCase):
         add_timesignatures_to_vocab(tok, [3, 4, 5])
         expected_vocab = prev_vocab + 3
         self.assertEqual(tok.vocab_size, expected_vocab)
-        self.assertTrue("TIMESIGNATURECUSTOM_34" in tok.vocab.keys())
-        self.assertTrue("TIMESIGNATURECUSTOM_44" in tok.vocab.keys())
-        self.assertTrue("TIMESIGNATURECUSTOM_54" in tok.vocab.keys())
+        expected = ["TIMESIGNATURECUSTOM_34", "TIMESIGNATURECUSTOM_44", "TIMESIGNATURECUSTOM_54"]
+        for expect in expected:
+            self.assertTrue(expect in tok.vocab)
+            # self.assertTrue(expect in tok.special_tokens)
 
     def test_add_pianists(self):
         tok = MIDILike(TokenizerConfig(**DEFAULT_TOKENIZER_CONFIG))
@@ -121,6 +95,7 @@ class TokenizerTest(unittest.TestCase):
         ]
         for expect in expected_pianist_tokens:
             self.assertTrue(expect in tok.vocab)
+            # self.assertTrue(expect in tok.special_tokens)
         # We should not add the following pianists: they're in our EXCLUDE list
         not_expected = ["PIANIST_DougMcKenzie", "PIANIST_JJAPianist1"]
         for not_expect in not_expected:
@@ -151,6 +126,7 @@ class TokenizerTest(unittest.TestCase):
         ]
         for expected in expected_genres:
             self.assertTrue(expected in tok.vocab)
+            # self.assertTrue(expected in tok.special_tokens)
         # These are the genres we're not expecting to add
         not_expected_genres = [
             # from Beegie Adair's artist metadata, but will be removed/merged with other genres
@@ -167,6 +143,8 @@ class TokenizerTest(unittest.TestCase):
         add_genres_to_vocab(tokfactory, js_fps)
         tok_genres = sorted(set([i for i in tokfactory.vocab.keys() if "GENRES" in i]))
         self.assertEqual(len(tok_genres), 26)
+        # tok_genres = sorted(set([i for i in tokfactory.special_tokens if "GENRES" in i]))
+        # self.assertEqual(len(tok_genres), 26)
 
     @unittest.skipIf(os.getenv("REMOTE") == "true", "Skipping test on GitHub Actions")
     def test_add_all_pianists(self):
@@ -175,6 +153,8 @@ class TokenizerTest(unittest.TestCase):
         add_pianists_to_vocab(tokfactory, js_fps)
         tok_pianists = sorted(set([i for i in tokfactory.vocab.keys() if "PIANIST" in i]))
         self.assertEqual(len(tok_pianists), 129)  # pijama + JTD pianists
+        # tok_pianists = sorted(set([i for i in tokfactory.special_tokens if "PIANIST" in i]))
+        # self.assertEqual(len(tok_pianists), 129)  # pijama + JTD pianists
 
 
 if __name__ == '__main__':
