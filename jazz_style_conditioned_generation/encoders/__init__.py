7c30b8cf7e87a18b96ba44566fac11ffe221c5ee
Huw Cheston <hwc31@cam.ac.uk>
2025-04-01 17:16:47 +0100

    add resnet to encoders/__init__.py

--- jazz_style_conditioned_generation/encoders/__init__.py
+++ jazz_style_conditioned_generation/encoders/__init__.py
@@ -1,2 +1,3 @@
 from .music_transformer import MusicTransformer
+from .resnet import ResNet50, load_performer_identifier
 from .scheduler import MusicTransformerScheduler, DummyScheduler, WarmupScheduler
